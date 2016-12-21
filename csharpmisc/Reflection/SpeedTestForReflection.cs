34ece8964ea26a5ddbb967a2e6e81ebb7c1c3d28
Amar Kumar Dubedy <amdubedy@microsoft.com>
2016-12-21 09:30:58 -0800

    Added Speed test for reflection

---
+++ csharpmisc/Reflection/SpeedTestForReflection.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpmisc.Reflection
+{
+    public class SpeedTestForReflection
+    {
+        public long CreateNormalList(int iterations)
+        {
+            var timer = Stopwatch.StartNew();
+            for(int i = 0; i < iterations; i++)
+            {
+                var list = new List<int>();
+            }
+
+            return timer.ElapsedMilliseconds;
+        }
+
+        public long CreateReflectedList(int iterations)
+        {
+            Type listType = typeof(List<int>);
+            var timer = Stopwatch.StartNew();
+            for (int i = 0; i < iterations; i++)
+            {
+                var list = Activator.CreateInstance(listType);
+            }
+
+            return timer.ElapsedMilliseconds;
+        }
+
+        public long AddToNormalList(int iterations)
+        {
+            var list = new List<int>();
+            var timer = Stopwatch.StartNew();
+            for(int i = 0; i < iterations; i++)
+            {
+                list.Add(i);
+            }
+
+            return timer.ElapsedMilliseconds;
+        }
+
+        public long AddToReflectedList(int iterations)
+        {
+            var list = new List<int>();
+            Type listType = typeof(List<int>);
+            Type[] parameterTypes = { typeof(int) };
+            MethodInfo mi = listType.GetMethod("Add", parameterTypes);
+            var timer = Stopwatch.StartNew();
+            for (int i = 0; i < iterations; i++)
+            {
+                mi.Invoke(list, new object[] { i });
+            }
+
+            return timer.ElapsedMilliseconds;
+        }
+    }
+}
---
+++ csharpmisctest/Reflection/SpeedTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using csharpmisc.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace csharpmisctest.Reflection
+{
+    [TestClass]
+    public class SpeedTest
+    {
+        [TestMethod]
+        public void TestCreationTimeForReflection()
+        {
+            SpeedTestForReflection sp = new SpeedTestForReflection();
+            var timeTakenWithoutReflection = sp.CreateNormalList(1000000);
+            var timeTakenWithReflection = sp.CreateReflectedList(1000000);
+            Console.WriteLine("Without " + timeTakenWithoutReflection);
+            Console.WriteLine("With " + timeTakenWithReflection);
+            Assert.IsTrue(timeTakenWithReflection > timeTakenWithoutReflection);
+        }
+
+        [TestMethod]
+        public void TestAdditionTimeForReflection()
+        {
+            SpeedTestForReflection sp = new SpeedTestForReflection();
+            var timeTakenWithoutReflection = sp.AddToNormalList(1000000);
+            var timeTakenWithReflection = sp.AddToReflectedList(1000000);
+            Console.WriteLine("Without " + timeTakenWithoutReflection);
+            Console.WriteLine("With " + timeTakenWithReflection);
+            Assert.IsTrue(timeTakenWithReflection > timeTakenWithoutReflection);
+        }
+    }
+}
