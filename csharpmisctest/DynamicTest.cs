5af4b3e68ffc4e6dc40ebbb4784bf6d2779b106c
Amar Kumar Dubedy <amdubedy@microsoft.com>
2016-12-23 13:54:10 -0800

    Missed changes

--- csharpmisctest/DynamicTest.cs
+++ csharpmisctest/DynamicTest.cs
@@ -6,11 +6,14 @@ namespace csharpmisctest
     using System.Collections;
     using System.Collections.Generic;
     using System.Dynamic;
+    using System.Reflection;
     using System.Runtime.Remoting;
     using System.Text;
 
+    using csharpmisc.Dynamic;
+
     [TestClass]
-    public class Dynamic
+    public class DynamicTest
     {
         [TestMethod]
         public void TestExpandoObject()
@@ -48,6 +51,38 @@ namespace csharpmisctest
 
             // Call the dynamically added functions
             rd.Print();
+        }
+
+        [TestMethod]
+        public void TestDynamicVsReflection()
+        {
+            // Using reflection
+            StringBuilder sb = new StringBuilder();
+            sb.GetType()
+                .InvokeMember("AppendLine", BindingFlags.InvokeMethod, null, sb, new object[] { "Reflection !" });
+
+            Console.WriteLine(sb);
+
+            // using Dynamic
+            StringBuilder dsb = new StringBuilder();
+            dynamic dynamicsb = (dynamic)dsb;
+            dynamicsb.AppendLine("Dynamic !");
+            Console.WriteLine(dsb);
+        }
+
+        [TestMethod]
+        public void TestUnifiedNumericMethods()
+        {
+            double double1 = 10, double2 = 20;
+            int int1 = 11, int2 = 22;
+
+            Console.WriteLine($"CommonMathDynamic basic version{CommonMathDynamic.Add(double1, double2)}");
+            Console.WriteLine($"CommonMathDynamic basic version{CommonMathDynamic.Add(int1, int2)}");
+
+            // Throws run-time error
+            // string result = CommonMathDynamic.Add(1,4) ; because it can't convert dynamic int result to string
+
+
         }
     }
 }
