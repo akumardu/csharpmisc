8c73bd6cb71416fecfd0f89e784789018b52ff64
Amar Kumar Dubedy <amdubedy@microsoft.com>
2017-01-04 07:13:02 -0800

    Elastic Search basic module

---
+++ csharpmisc/ElasticSearch/ESImpl.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nest;
+
+namespace csharpmisc.ElasticSearch
+{
+    public class ESImpl
+    {
+        private Uri node;
+        private ConnectionSettings settings;
+        private ElasticClient client;
+
+        public ESImpl(string uri)
+        {
+            node = new Uri(uri);
+
+            // We can setup default index in ConnectionSettings
+            settings = new ConnectionSettings(node)
+                       .DefaultIndex("defaultindex");
+
+            // Setup exact indices for known data types 
+            settings.MapDefaultTypeIndices(m => m
+                                                .Add(typeof(Post), "blog_post"));
+
+            client = new ElasticClient(settings);
+        }
+
+        public bool CreateIndex(string index, int numberOfReplicas=1, int numberOfShards=1)
+        {
+            var response = client.CreateIndex(index, c => c
+                                                        .Settings(s => s
+                                                                        .NumberOfReplicas(numberOfReplicas)
+                                                                        .NumberOfShards(numberOfShards)
+                                                                  )
+                                                         .Mappings(m => m
+                                                                        .Map<Post>(d => d.AutoMap()))
+                                               );
+                                                                        
+            if(!response.IsValid)
+            {
+                Console.WriteLine($"CreateIndex failed with {response.OriginalException}");
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool DeleteIndex(string index)
+        {
+            var deleteResponse = client.DeleteIndex(index);
+            return deleteResponse.IsValid;
+        }
+
+        public IEnumerable<string> GetIndex(string index)
+        {
+            var getIndex = client.GetIndex(index);
+            return getIndex?.Indices?.Keys;
+        }
+
+        public IClusterHealthResponse ClusterHealth()
+        {
+            var clusterHealth = client.ClusterHealth();
+            return clusterHealth;
+        }
+
+        public bool PostDocuments(Post doc)
+        {
+            var result = client.Index<Post>(doc);
+            return result.IsValid;
+        }
+
+        public bool DeleteDocuments(Post doc)
+        {
+            var result = client.Delete<Post>(doc);
+            return result.IsValid;
+        }
+
+        public IEnumerable<Post> SearchAllDocuments()
+        {
+            var result = client.Search<Post>(s => s
+                                                   .Query(q => q.MatchAll())
+                                                    );
+            var posts = new List<Post>();
+            if (result.IsValid)
+            {
+                foreach(var doc in result.Documents)
+                {
+                    posts.Add(doc);
+                }
+                return posts;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ csharpmisctest/ElasticSearch/ESImplTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using csharpmisc.ElasticSearch;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace csharpmisctest.ElasticSearch
+{
+    [TestClass]
+    public class ESImplTest
+    {
+        private readonly string ESUri = "http://localhost:9200";
+
+        [TestMethod]
+        public void TestSetup()
+        {
+            var es = new ESImpl(ESUri);
+            var clusterHealth = es.ClusterHealth();
+            Console.WriteLine(clusterHealth);
+            Assert.IsTrue(clusterHealth.IsValid);
+        }
+
+        [TestMethod]
+        public async Task TestIndexCreation()
+        {
+            var es = new ESImpl(ESUri);
+            var initialClusterHealth = es.GetIndex("blog_post");
+            Assert.IsTrue(initialClusterHealth == null || initialClusterHealth.Count() == 0, "Initial cluster health is invalid");
+            var createResult = es.CreateIndex("blog_post");
+            Assert.IsTrue(createResult, "Index wasn't created");
+            var finalClusterHealth = es.GetIndex("blog_post");
+            await Task.Delay(1000);
+            Assert.IsTrue(finalClusterHealth != null && finalClusterHealth.Count() == 1, "Final cluster health is invalid");
+            var deleteResult = es.DeleteIndex("blog_post");
+            Assert.IsTrue(deleteResult, "Delete didn't work");
+        }
+
+        [TestMethod]
+        public async Task TestPostAndSearchMessage()
+        {
+            var es = new ESImpl(ESUri);
+            var createResult = es.CreateIndex("blog_post");
+            Assert.IsTrue(createResult, "Index wasn't created");
+            var searchResults = es.SearchAllDocuments();
+            var postResult = es.PostDocuments(new Post()
+            {
+                UserId = 1,
+                PostDate = DateTime.UtcNow,
+                PostText = "This is an awesome post"
+            });
+            Assert.IsTrue(postResult);
+            await Task.Delay(1000);
+            searchResults = es.SearchAllDocuments();
+            Assert.IsTrue(searchResults != null && searchResults.Count() == 1, "Search failed");
+            var deleteResult = es.DeleteIndex("blog_post");
+            Assert.IsTrue(deleteResult, "Delete didn't work");
+        }
+    }
+}
